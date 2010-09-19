56292e13128be5a5dc2177b7e20e99127e969f0d
Michael De Soto <web@quarg.com>
2010-09-18 20:09:34 -0400

    Forked jQuery captify plugin and renamed it to Cutline. Cleaned up inline comments, and changed default properties to fit my needs. New minified version is slightly smaller than preforked version.

--- jquery-cutline.js
+++ jquery-cutline.js
@@ -1,120 +1,134 @@
-/* jQuery Captify Plugin
- * 
- * Copyright (C) 2008 Brian Reavis
- * Licenced under the MIT license
- * 
- * $Date: 2008-12-12 [Fri, 12 Dec 2008] $
- */
+// jQuery Cutline Plugin
+// Copyright (C) 2010 Michael De Soto.
+// Copyright (C) 2009 Caius Durling.
+// Copyright (C) 2008 Brian Reavis.
+//
+// http://github.com/quarghost/jQuery-Cutline
+// $Date$
 
 jQuery.fn.extend({
-	captify: function(o){
-		var o = $.extend({
-			speedOver: 'fast',		// speed of the mouseover effect
-			speedOut: 'normal',		// speed of the mouseout effect
-			hideDelay: 500,		  	// how long to delay the hiding of the caption after mouseout (ms)
-			animation: 'fade',	  	// 'fade' or 'slide'
-			prefix: '',			  	// text/html to be placed at the beginning of every caption
-			className: 'caption'	// the name of the CSS class to apply to the caption box
-		}, o);
-		$(this).each(function(){
-			var img = this;
-			$(this).load(function(){
-				$this = img;
-				if (this.hasInit){
-					return false;
-				}
-				this.hasInit = true;
-				var over_caption = false;
-				var over_img 	 = false;
-				//pull the label from another element if there if there is a 
-				//valid element id inside the rel="..." attribute, otherwise,
-				//just use the text in the title="..." attribute.
-				var captionLabelSrc = $('#' + $(this).attr('rel'));
-				var captionLabelTitle = !captionLabelSrc.length ? $(this).attr('title') : captionLabelSrc.html();
-				var captionLabelHTML = !captionLabelTitle.length ? $(this).attr('alt') : captionLabelTitle;
-				captionLabelSrc.remove();
-				var toWrap = this.parent && this.parent.tagName == 'a' ? this.parent : $(this);
-				var wrapper = toWrap.wrap('<div></div>').parent();
-				wrapper.css({
-					overflow: 'hidden',
-					padding: 0,
-					fontSize: 0.1
-				})
-				wrapper.addClass('caption-wrapper');
-				wrapper.width($(this).width());
-				wrapper.height($(this).height());
-				//transfer the border properties from the image to the wrapper
-				$.map(['top','right','bottom','left'], function(i){
-					$.map(['style','width','color'], function(j){
-						var key = 'border-'+i+'-'+j;
-						wrapper.css(key, $(img).css(key));
-					});
-				});
-				$(img).css({border: '0 none'});
-				//transfer the margin properties
-				$.map(['top','right','bottom','left'], function(t){
-					var key = 'margin-'+t;
-					wrapper.css(key, $(img).css(key));
-				});
-				
-				//create two consecutive divs, one for the semi-transparent background,
-				//and other other for the fully-opaque label
-				var caption 		= $('div:last', wrapper.append('<div></div>')).addClass(o.className);
-				var captionContent 	= $('div:last', wrapper.append('<div></div>')).addClass(o.className).append(o.prefix).append(captionLabelHTML);
-				
-				//override hiding from CSS, and reset all margins (which could have been inherited)
-				$('*',wrapper).css({margin: 0}).show();
-				//ensure the background is on bottom
-				var captionPositioning = jQuery.browser.msie ? 'static' : 'relative';
-				caption.css({
-					zIndex: 1,
-					position: captionPositioning
-				});
-				
-				//clear the backgrounds/borders from the label, and make it fully-opaque
-				captionContent.css({
-					position: captionPositioning,
-					zIndex: 2,
-					background: 'none',
-					border: '0 none',
-					opacity: 1.0
-				});
-				caption.width(captionContent.outerWidth());
-				caption.height(captionContent.outerHeight());
-				
-				//pull the label up on top of the background
-				captionContent.css({ 'marginTop': -caption.outerHeight() });
-				//function to push the caption out of view
-				var cHide = function(){
-					if (!over_caption && !over_img)
-						caption.animate({ marginTop: 0 }, o.speedOut); 
-				};
-				//when the mouse is over the image
-				$(this).hover(
-					function(){ 
-						over_img = true;
-						if (!over_caption) {
-							caption.animate({
-								marginTop: -caption.height()
-							}, o.speedOver);
-						}
-					}, 
-					function(){ 
-						over_img = false;
-						window.setTimeout(cHide, o.hideDelay);
-					}
-				);
-				
-				//when the mouse is over the caption on top of the image (the caption is a sibling of the image)
-				$('div', wrapper).hover(
-					function(){ over_caption = true; },
-					function(){ over_caption = false; window.setTimeout(cHide, o.hideDelay); }
-				);
-			});
-			//if the image has already loaded (due to being cached), force the load function to be called
-			if (this.complete || this.naturalWidth > 0){
-				$(img).trigger('load');
-			}
-		});
-	}
+    cutline: function(oo) {
+        var o = $.extend({
+            speedOver: 'fast',      // Mouseover effect speed.
+            speedOut: 'normal',     // Mouseout effect speed.
+            hideDelay: 2500,        // Delay in ms before hiding cutline.
+            animation: 'slide',     // Choose 'fade' or 'slide'.
+            prefix: '',             // Prepend text/html to cutline.
+            className: 'cutline'    // CSS class applied to cutline.
+        }, oo);
+
+        $(this).each(function(){
+            var img = this;
+            $(this).load(function(){
+                $this = img;
+                if (this.hasInit){
+                    return false;
+                }
+                this.hasInit = true;
+                var cutlineHover = false;
+                var imgHover = false;
+
+                // Use cutline in alternative element specified in rel when it
+                // exists. Otherwise use copy specified in title attribute. We
+                // fallback on alt attribute when title attribute is empty.
+                var cutlineRel = $('#' + $(this).attr('rel'));
+                var cutlineTitle = !cutlineRel.length ? $(this).attr('title') : cutlineRel.html();
+                var cutlineAlt = !cutlineTitle.length ? $(this).attr('alt') : cutlineTitle;
+                cutlineRel.remove();
+
+                var toWrap = this.parent && this.parent.tagName == 'a' ? this.parent : $(this);
+                var wrapper = toWrap.wrap('<div></div>').parent();
+                wrapper.css({
+                    overflow: 'hidden',
+                    padding: 0,
+                    fontSize: 0.1
+                });
+                wrapper.addClass('cutline-wrapper');
+                wrapper.width($(this).width());
+                wrapper.height($(this).height());
+
+                // Use border properties from img on wrapper.
+                $.map(['top', 'right', 'bottom', 'left'], function(i){
+                    $.map(['style', 'width', 'color'], function(j){
+                        var key = 'border-' + i + '-' + j;
+                        wrapper.css(key, $(img).css(key));
+                    });
+                });
+
+                $(img).css({border: '0 none'});
+
+                // Transfer margin properties from img to wrapper
+                $.map(['top', 'right', 'bottom', 'left'], function(t){
+                    var key = 'margin-' + t;
+                    wrapper.css(key, $(img).css(key));
+                });
+
+                // In order to keep the cutline copy readable when using a 
+                // translucent background, we keep the two separated.
+                var cutline = $('div:last', wrapper.append('<div></div>')).addClass(o.className);
+                var cutlineCopy = $('div:last', wrapper.append('<div></div>')).addClass(o.className).append(o.prefix).append(cutlineAlt);
+
+                // Reset inherited margins and show cutline.
+                $('*', wrapper).css({margin: 0}).show();
+
+                // Be sure background div is on bottom of stack.
+                var cutlinePosition = jQuery.browser.msie ? 'static' : 'relative';
+                cutline.css({
+                    zIndex: 1,
+                    position: cutlinePosition
+                });
+
+                // Clear background and border properties from cutline copy.
+                // Here we also make sure copy is fully opaque.
+                cutlineCopy.css({
+                    position: cutlinePosition,
+                    zIndex: 2,
+                    background: 'none',
+                    border: '0 none',
+                    opacity: 1.0
+                });
+                cutline.width(cutlineCopy.outerWidth());
+                cutline.height(cutlineCopy.outerHeight());
+
+                // Make sure copy sits on top of background.
+                cutlineCopy.css({ 'marginTop': -cutline.outerHeight() });
+
+                // Function pushing cutline from view.
+                var cutlineHide = function(){
+                    if (!cutlineHover && !imageHover)
+                        cutline.animate({ marginTop: 0 }, o.speedOut); 
+                };
+                
+                $(this).hover(
+
+                    // Mouseover event.
+                    function(){ 
+                        imageHover = true;
+                        if (!cutlineHover) {
+                            cutline.animate({
+                                marginTop: -cutline.height()
+                            }, o.speedOver);
+                        }
+                    },
+
+                    // Mouseout event.
+                    function(){ 
+                        imageHover = false;
+                        window.setTimeout(cutlineHide, o.hideDelay);
+                    }
+                );
+
+                // When mouse over cutline, cutline is sibling of image.
+                $('div', wrapper).hover(
+                    function(){ cutlineHover = true; },
+                    function(){ cutlineHover = false; window.setTimeout(cutlineHide, o.hideDelay); }
+                );
+            });
+
+            // If image already loaded, force invocation of load function.
+            if (this.complete || this.naturalWidth > 0){
+                $(img).trigger('load');
+            }
+        });
+    }
 });
